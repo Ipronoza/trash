9dc6372a834de208dca3aefa3b6936f82c76c41e
Ipronoza <ipron1988@gmail.com>
2023-03-11 12:22:39 +0200

    add animation

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
     document.querySelector("body").classList.toggle("blue_color");
     // console.log('zzzz');
  })
+
+ let animDiv = document.createElement("div");
+ animDiv.classList = "anim_div";
+ document.body.append(animDiv);
